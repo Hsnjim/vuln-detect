adaee172af3961ec98f34e5087b920ac7ed72f8d
Hsnjim <jimhessanon@gmail.com>
2023-05-07 21:51:25 +0200

    bug_on_code_fixed

--- dynamic-analysis.py
+++ dynamic-analysis.py
@@ -28,28 +28,35 @@ with open(urls_file_path, "r") as f:
 # Port utilisé par ZAP
 zap_port = 8080
 
-def scan_app(zap, app_url):                                                                        #Le scannage avec zaproxy se fait en deux étapes : Le spidering et le active scan
-    zap.urlopen(app_url)                                                                           #Le spidering en fait c'est l'étape oû le zap explore tout les répertoires existants dans l'application web.
-    time.sleep(2)                                                                                  #Le active scan est l'étape du scannage proprement dit. Le scannage de tout les répertoires énumérés par le spider
-                                                                                                   #La commande qui lance le spidering est zap.spider.scan() 
+def scan_app(zap, app_url):                                                                        
+    zap.urlopen(app_url)                                                                           
+    time.sleep(10)                                                                                  
+
+    print("Spidering starting .....")                                                                                           
     spider_id = zap.spider.scan(url=app_url, apikey=api_key) 
-    print(spider_id)                                      #La commande zap.spider.status() affiche en fait le pourcentage d'évolution du spider
-    while int(zap.spider.status(spider_id)) < 100:                                                          #Donc tant que le statut n'est pas venu a 100%, l'opération s'arrête pas.
-                                                                                                   #Le time sleep c'est pour dire au script d'attendre un peu avant de vérifier à nouveau le pourcentage de l'opération en cours
-        time.sleep(5)                                                                              #Ca permet d'éviter de surcharger l'API de Zaproxy avec des requêtes assez fréqeuntes.
+    time.sleep(5)                                     
+    while int(zap.spider.status(spider_id)) < 100:   
+        print("Pourcentage d'execution du spidering " + zap.spider.status(spider_id) + "%")                                                                                                                                                
+        time.sleep(2)                                                                              
     print("Spider completed.")
 
-    asscan_id = zap.ascan.scan(url=app_url, apikey=api_key)
-    while int(zap.ascan.status(asscan_id)) < 100:
-        time.sleep(5)
-    print("Asscan completed")
+    print("Active scan starting .....")
+    ascan_id = zap.ascan.scan(url=app_url, apikey=api_key)
+    while int(zap.ascan.status(ascan_id)) < 100:
+        print("Pourcentage d'execution du ascan " + zap.ascan.status(ascan_id) + "%")         
+        time.sleep(2)
+    print("Ascan completed")
 
 def generate_report(zap, app_url):
+    print("Génération de rapport en cours ....")
     report_file = "{}/{}_report.html".format(reports_folder, app_url.replace("://", "_").replace("/", "_"))
     with open(report_file, "wb") as f:
-        f.write(zap.core.htmlreport(api_key))
+        f.write(zap.core.htmlreport(api_key).encode('utf-8'))
+    print("Rapport généré")
+        
 
-def plot_alerts_by_risk(zap):
+def plot_alerts_by_risk(zap, app_url):
+    print("Génération du graphe en cours ....")
     risk_levels = {"Informational": 0, "Low": 0, "Medium": 0, "High": 0}
     alerts = zap.core.alerts()
 
@@ -62,17 +69,20 @@ def plot_alerts_by_risk(zap):
     plt.xlabel("Niveaux de risque")
     plt.ylabel("Nombre d'alertes")
     plt.title("Nombre d'alertes par niveau de risque")
-    plt.savefig("{}/alertes_par_niveau_de_risque.png".format(graphs_folder))
-    plt.show()
+    plt.savefig("{}/{}_alertes.png".format(graphs_folder, app_url.replace("://", "_").replace("/", "_")))
+    print("Graphe généré")
 
 def main():
-    zap = ZAPv2(proxies={"http": "http://localhost:{}".format(zap_port), "https": "http://localhost:{}".format(zap_port)})
+    zap = ZAPv2(apikey=api_key)
     
     for app_url in apps:
         print("Scannage de l'application : {}".format(app_url))
         scan_app(zap, app_url)
         generate_report(zap, app_url)
-        plot_alerts_by_risk(zap)
+        plot_alerts_by_risk(zap, app_url)
+
+        print("\n")
+        print("******************************************************")
 
 if __name__ == "__main__":
     main()
